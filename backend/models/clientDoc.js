58b0cd70df4aebb3b6cbd4e272dec272f1d5ceea
turnkeyz <34150538+turnkeyz@users.noreply.github.com>
2022-02-18 23:40:25 -0800

    added two more models for backend

---
+++ backend/models/clientDoc.js
@@ -0,0 +1,41 @@
+const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let clientDocSchema = new Schema({
+    _id: {
+        type: String,
+        default: uuid.v1
+    },
+    clientID: {
+        type: Number,
+        required: true
+    },
+    ssn: {
+        type: Number,
+        required: true
+    },
+    driverLicense:{
+        type:Number,
+        required:true
+    },
+    otherDoc:{
+        type:null,
+        required:false
+    }
+},
+    {
+        collection: 'clientDoc'
+    });
+    
+module.exports = mongoose.model('clientDoc', clientDocSchema)
+
+// Designation:
+// 1. clientID: Number (Link the activity to client)
+// 2. program: String (Dropdown List: Adult Education / Family Support Services / Early Childhood / Youth Services)
+// 3. shortNotes: String
+// 4. datetime: Date
+// 5. timeSpend: Number
+// 6. workID: relationship manager
+// 7. hasUsedServices: Boolean (true | false)
+// 8. handlingStatus: String (Not at all | Partially | Fully)
