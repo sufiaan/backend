74589b145ee121f6130680cb5ceda4e427b3cba1
turnkeyz <34150538+turnkeyz@users.noreply.github.com>
2022-02-19 00:05:46 -0800

    added more models

---
+++ backend/models/employment.js
@@ -0,0 +1,49 @@
+const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let employmentSchema = new Schema({
+    _id: {
+        type: String,
+        default: uuid.v1
+    },
+    clientId: {
+        type: Number,
+        required: true
+    },
+    employmentStatus:{
+        type:Array,
+        required:true
+    },
+    length:{
+        type:Array,
+        required:false
+    },
+    employer:{
+        type:String,
+        required:false
+    },
+    occupation:{
+        type:String,
+        required:false
+    },
+    modifyAt:{
+        type:null,
+        required:false
+    }
+},
+    {
+        collection: 'employment'
+    });
+    
+module.exports = mongoose.model('employment', employmentSchema)
+
+// Designation:
+// 1. clientID: Number (Link the activity to client)
+// 2. program: String (Dropdown List: Adult Education / Family Support Services / Early Childhood / Youth Services)
+// 3. shortNotes: String
+// 4. datetime: Date
+// 5. timeSpend: Number
+// 6. workID: relationship manager
+// 7. hasUsedServices: Boolean (true | false)
+// 8. handlingStatus: String (Not at all | Partially | Fully)
